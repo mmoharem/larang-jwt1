be1bd7e968872e74830ad18ec9691ae410bb6f0d
mmoharem <cloud@bnfstore.com>
2018-06-22 04:01:36 +0200

    finish setting jwt

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
 
 /*
 |--------------------------------------------------------------------------
