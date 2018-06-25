b989c2732ed3047123a298bdad71576c48a54d7c
mmoharem <cloud@bnfstore.com>
2018-06-25 09:48:58 +0200

    continue

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SignupRequest;
+use App\User;
+
 
 class AuthController extends Controller
 {
@@ -14,7 +17,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login', 'signup']]);
     }
 
     /**
@@ -27,10 +30,20 @@ class AuthController extends Controller
         $credentials = request(['email', 'password']);
 
         if (! $token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'email or password doesnt exist'], 401);
         }
 
         return $this->respondWithToken($token);
+    }
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function signup(SignupRequest $request)
+    {
+       User::create($request->all());
+    //    return $this->login($request);
     }
 
     /**
@@ -41,6 +54,7 @@ class AuthController extends Controller
     public function me()
     {
         return response()->json(auth()->user());
+        
     }
 
     /**
